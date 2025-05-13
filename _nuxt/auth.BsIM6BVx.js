b2b22bfb9ed32667463bc83f3468cebd5caaab79
rustamnew <36161445+rustamnew@users.noreply.github.com>
2025-05-13 17:21:20 +0300

    test

--- _nuxt/auth.BsIM6BVx.js
+++
@@ -1,1 +0,0 @@
-import{aN as e,ar as u,m as s,aO as o}from"./entry.DCNB1XUa.js";const h=e(async t=>{const a=u();if(!s(()=>a.authenticated).value&&t.path!=="/auth")return o("/auth")});export{h as default};
--- _nuxt/data.CyXGuqNC.js
+++
@@ -1,1 +0,0 @@
-import{aT as o,g as a}from"./entry.DCNB1XUa.js";const s=o("dataStore",()=>{const t=a({}),e=a(!1);return{data:t,loading:e}});export{s as u};
